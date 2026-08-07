79eb652603ced83e990a86ce9adab7312ce0604b
Putra Harapan Tafonao <putraharapantafonao199@gmail.com>
2026-08-08 00:04:39 +0700

    Change outDir to out to definitively bypass Vercel dist ignore rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  outDir: './out',
   server: {
     port: 4322,
     host: true
