769d6292392f07662961ba5eabe2953f4b27e360
Putra Harapan Tafonao <putraharapantafonao199@gmail.com>
2026-08-08 01:00:59 +0700

    Revert outDir change in astro.config.mjs which broke Vercel's expected build output structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  outDir: './my_server_build',
   server: {
     port: 4322,
     host: true
